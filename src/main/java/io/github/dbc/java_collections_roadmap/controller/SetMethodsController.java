05a17ca19cc885105b63290590db3eaa0b528f0b
vaidik22 <vaidik.nigam_cs19@gla.ac.in>
2022-04-08 01:30:28 +0530

    Attach methods to button

---
+++ src/main/java/io/github/dbc/java_collections_roadmap/controller/SetMethodsController.java
@@ -0,0 +1,10 @@
+/*
+ * Author Name: Vaidik Nigam
+ * IDE: Intellij IDEA Community Edition
+ * Date: 08-04-2022
+ */
+
+package io.github.dbc.java_collections_roadmap.controller;
+
+public class SetMethodsController {
+}
